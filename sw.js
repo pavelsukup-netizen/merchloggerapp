897161d403809f01f806e9935096bd89acfe60aa
pavelsukup-netizen <pavel.sukup@gmail.com>
2026-01-22 20:50:03 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const SW_VERSION = "2026-01-23";
+const SW_VERSION = "2026-01-24";
 // sw.js — cache + offline. BUMP VER při každé změně souborů.
-const CACHE = "mv_mobile_logger_v23";
+const CACHE = "mv_mobile_logger_v24";
 
 const ASSETS = [
   "./",
